0a6f9e078c6e6397a7f0f1347b1d2e719e1d5870
Daniel Prokhorov <d.prokhorov@sparks-gmbh.de>
2024-08-02 18:25:42 +0200

    added netstat and openssh server within the Dockerfile for external connections

--- Appium/start.sh
+++ Appium/start.sh
@@ -1,41 +1,14 @@
 #!/bin/bash
 
-# It is workaround to access adb from androidusr
-echo "Prepare adb to have access to device"
-sudo /opt/android/platform-tools/adb devices >/dev/null
-sudo chown -R 1300:1301 .android
-echo "adb can be used now"
+# Start SSH service
+sudo service ssh start
 
-# Connect device via wireless
-if [ "${REMOTE_ADB}" = true ]; then
-	echo "Connect device via wireless"
-	# Avoid lost connection
-	${APP_PATH}/wireless_autoconnect.sh && \
-	${APP_PATH}/wireless_connect.sh
-fi
+# Wait for port 5555 to be opened/forwarded
+echo "Waiting for port 5555 to be opened..."
+while ! nc -z localhost 5555; do
+  sleep 1
+done
 
-# Command to start Appium
-APPIUM_LOG="${APPIUM_LOG:-/var/log/appium.log}"
-command="xvfb-run appium --log $APPIUM_LOG"
-
-# Adding Selenium configurations if needed
-if [ "${CONNECT_TO_GRID}" = true ]; then
-	NODE_CONFIG_JSON="${APP_PATH}/nodeconfig.json"
-	if [ "${CUSTOM_NODE_CONFIG}" != true ]; then
-		${APP_PATH}/generate_selenium_config.sh ${NODE_CONFIG_JSON}
-	fi
-	command+=" --nodeconfig ${NODE_CONFIG_JSON}"
-fi
-
-if [ "${DEFAULT_CAPABILITIES}" = true ]; then
-	DEFAULT_CAPABILITIES_JSON="${APP_PATH}/defaultcapabilities.json"
-	command+=" --default-capabilities ${DEFAULT_CAPABILITIES_JSON}"
-fi
-
-# Adding additional Appium configuration if needed
-command+=" ${APPIUM_ADDITIONAL_PARAMS}"
-
-# Run the whole command
-pkill -x xvfb-run
-rm -rf /tmp/.X99-lock
-${command}
+# Once port 5555 is open, start the Appium script
+echo "Port 5555 is open, starting Appium..."
+${APP_PATH}/start_appium.sh
